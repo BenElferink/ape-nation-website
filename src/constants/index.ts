0620dcdf5fb7110beb07a85786dca6d119f17280
Ben Elferink <ben.elferink@icloud.com>
2025-03-16 17:53:19 +0200

    update nation coin policy ID

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -19,7 +19,7 @@ export const APE_NATION_POLICY_ID = '58b9f55e6ea9828dea7a8d9f49420171c6360f99b5e
 export const JUNGLE_JUICE_POLICY_ID = 'a5c0b233312ff05cdd6271d5749992f77a8e568a0533855b250dc7a0'
 export const MUTATION_NATION_POLICY_ID = '929ab04657f37d1108beb720a73f55f83b65948e05ec3a5619fa7875'
 export const MUTATION_NATION_MEGA_MUTANTS_POLICY_ID = 'f8d619c4650033b4f3a4759805058c379ddc5d6b56102b948da5c162'
-export const NATION_COIN_POLICY_ID = 'cf5d945ad03a11c46e70a85daa8598b2275f9442ceed1249754ad9a1'
+export const NATION_COIN_POLICY_ID = '38a2f68f5b009b1303b2e71ba3a374b7a2da11b5910423e2cf989948'
 export const OG_CLUB_CARD_POLICY_ID = '21a44e25de32d0c2cba0413cb417fa61ed65cd9e2f419cf5c714ca4f'
 export const ORDINAL_TOKENS_POLICY_ID = 'c9fa37158ded418a5bb18159e4e7d7c1aeadd202ef7eaa84fbf23350'
 export const BLING_POLICY_ID = 'b0fd2efcb7e8b5d0dd6fce7403e31afc7919efe4e68a66570a5ee055'
@@ -39,7 +39,7 @@ export const LINKS = {
   MAGIC_EDEN_ORDINALS: 'https://magiceden.io/ordinals/marketplace/ordinals-by-ape-nation',
   NATION_TOKENOMICS: 'https://usdnation-token-by-ape-nation.gitbook.io/usdnation-token-by-ape-nation',
   NATION_TAPTOOLS:
-    'https://www.taptools.io/charts/token?pairID=0be55d262b29f564998ff81efe21bdc0022621c12f15af08d0f2ddb1.a7ecc694852dba715767f6be7e30c6ac1aab68baa9794815532e0da28b2fd80d',
+    'https://www.taptools.io/charts/token/f5808c2c990d86da54bfc97d89cee6efa20cd8461616359478d96b4c.f0e9e703b6b981d7740898bb023ef004976c988385fc8ecf173430a6c9ba7e51',
 }
 
 export const BLOODLINE_COLLATERAL_ADDRESS = 'addr1q8yh4nz600fqa5yqyt7nusqlq4cw65h4mcgufruf6gnjgzf34cqsunpd2hzxrz8glhlcwlce3rtxq4x7lvf3jcjmqgrqzynw52'
